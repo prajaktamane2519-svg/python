e6f8e20852ece8275fe146a1f955a5c62b93e2ea
Prajakta mane <prajaktamane2519@gmail.com>
2026-07-16 23:20:57 +0530

    variable program

---
+++ variables.py
@@ -0,0 +1,47 @@
+a=1
+print(a)
+b=1.56
+print(b)
+c="Shaarvi"
+print(c)
+x=10
+print(x+1)
+name="shaam"
+print("my name is :",name)
+age =12
+age2=age
+print(age)
+print(type(name))
+print(type(age))
+s=56.56
+print(s)
+print(type(s))
+a="ram"
+A="SHIV"
+print(a)
+print(A)
+a=12
+b=5
+sum=a+b
+print(sum)
+a=34
+b=5
+diff=a-b
+print(diff)
+#arithmetic operators
+a=5
+b=3
+sum=a+b
+print(sum)
+print(a/b)
+print(a%b)
+print(a**b) #a^b
+a=50
+b=20
+print(a==b)
+print(a!=b)
+print(a>b)
+#aassigment operator
+num=10
+num+=10
+print("num :",num)
