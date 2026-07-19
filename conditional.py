3ab3137dd60d66e36d8ba00a2e5ec6a2b12c9e50
Prajakta mane <prajaktamane2519@gmail.com>
2026-07-19 18:31:28 +0530

    conditional program

---
+++ conditional.py
@@ -0,0 +1,32 @@
+str1="this is a string"
+print(str1)
+
+str2 ="this is a string.\n we are creating it in python"
+print(str2)
+str3="this is a string.\t we are creating it in python"
+print(str3)
+str1 ="wakad"
+str2="pune"
+print(str1+str2) 
+str ="apple"
+print(str[-3:-1])
+str1="i am studying"
+print(str1.capitalize())
+a="sagar"
+print(a.replace("a", "o"))
+
+b="i live in city "
+print(str.find("i"))
+print(b.find("live"))
+
+print(b.count("i"))
+name=input("enter your name")
+
+
+
+
+
+
+
+
+  
