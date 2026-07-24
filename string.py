414445cadd67f106a2dfca0715afffd506f25585
Prajakta mane <prajaktamane2519@gmail.com>
2026-07-24 10:37:48 +0530

    string_index

--- string.py
+++ string.py
@@ -10,5 +10,12 @@ print("my name is %s and i am %d" %(name,age))
 name="rish"
 age=24
 print(F"my name is{name} and i am{age}")
-
-
+print("Hello\nworld")
+print("Hello\tworld")
+my_name="mahesh"
+print(my_name[0])
+print(my_name[1])
+print(my_name[2])
+print(my_name[3])
+print(my_name[4])
+print(my_name[5])
