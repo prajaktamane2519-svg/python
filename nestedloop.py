ea3e5f8ed8056115d2500a2c1321d8b35aee4def
Prajakta mane <prajaktamane2519@gmail.com>
2026-07-26 18:02:09 +0530

    forloop

--- nestedloop.py
+++ nestedloop.py
@@ -2,4 +2,12 @@ for i in range(1,11):
     print(i)
 for i in range(10,0,-1):
    print(i)
+for i in range(2,21,2):
+    print(i)
+for i in range(1,20,2):
+    print(i)
+    total=0
+for i in range(1,11):
+    total+=i
+print("sum =",total)
     
