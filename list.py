363df61987dfd03f379a37f40cf73db5a085a2c2
Prajakta mane <prajaktamane2519@gmail.com>
2026-07-27 12:33:38 +0530

    listprogaram

--- list.py
+++ list.py
@@ -3,3 +3,21 @@ print(my_list)
 my_list2=[1,2,[2,4],True,[4.,5,8]]
 print(my_list2)
 print(type(my_list2))
+my_list=[9,56,89]
+print(my_list)
+list=[10,20,30,40]
+print(list[0])
+print(list[1])
+print(list[-1])
+print(list[-2])
+print(list[0:3])
+print(list[1:5])
+print(list[-3: ])
+print(list[ :: ])
+print(list[ :: 2])
+my_list4=['apple','banana','cherry']
+print(my_list4)
+my_list4[1]='blueberry'
+print(my_list4)
+my_list4.append("mango")
+print(my_list4)
