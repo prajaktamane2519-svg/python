d0fe2122000e1b14c2a6e62cf8e63d77c3f3a54c
Prajakta mane <prajaktamane2519@gmail.com>
2026-07-27 11:52:48 +0530

    forloop

--- nestedloop.py
+++ nestedloop.py
@@ -10,4 +10,46 @@ for i in range(1,20,2):
 for i in range(1,11):
     total+=i
 print("sum =",total)
-    
+number=5
+for i in range(1,11):
+   print(number,"X",i,"=",number*i)
+name="shaarvi"
+for ch in name:
+    print(ch)
+    number=[10,20,56,89]
+for num in number:
+    print(num)
+for num in range(1,101):
+    if num%5==0:
+        print(num)
+for num in range(1,11):
+    print(num,"square =",num*num)
+    num=5
+    fact=1
+for i in range(1,num+1):
+    fact=fact*i
+print("factorial =",fact)
+text=input("Enter a string: ")
+count=0
+for ch in text.lower():
+   
+    if ch in "aeiou":
+        count +=1
+print("vowels =",count)
+numbers=[10,25,8,45,30]
+largest=numbers[0]
+for num in numbers:
+    if num>largest:
+        largest=num
+print("laergest=",largest)
+numbers=[10,5,46,78,98]
+smallest=numbers[0]
+for num in numbers:
+    if num< smallest:
+        smallest=num
+        print("smallest=",smallest)
+for num in range(2,101):
+    if num%i==0:
+        break
+else:
+     print(num)
