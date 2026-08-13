646a33f08991ffff173db2672457552f07f91dde
Prajakta mane <prajaktamane2519@gmail.com>
2026-08-13 19:49:36 +0530

    stringprogram

---
+++ stringp.py
@@ -0,0 +1,68 @@
+city="pune"
+print(city)
+sub='python is easy'
+print(sub)
+text="python"
+print(text[0])
+print(text[3])
+print(text[-2])
+print(text[ : 6])
+print(text[5: ])
+print(text[4])
+print(text[-2])
+print(text[0::6])
+print(text[ :6])
+print(text[-5: ])
+print(text[ ::-1])
+a="Hello"
+b="world"
+print(a+ ""+b)
+print(a*3)
+
+print(len(a))
+text="python programming"
+print(text.upper())
+print(text.lower())
+print(text.title())
+print(text.capitalize())
+print(text.replace("python","java"))
+print(text.split())
+print(text.find("pro"))
+print(text.find("python"))
+text="python123"
+print(text.isalpha())
+print(text.isdigit())
+print(text.isalnum())
+print(f"cours name is{text}.")
+text="python is easy"
+print(text)
+text='shaarvi mane'
+print(text)
+text="python programming"
+print(text[0:6])
+print(text[7:])
+print(text[:6])
+print(text[-5:])
+print(text[::-1])
+a="Hello"
+b="world"
+print(a+""+b)
+first_name="praj"
+last_name="mane"
+print(first_name+""+last_name)
+a="i"
+b="love"
+c="python"
+print(a+""+b+""+c)
+text="python"
+print(len(text))
+t="Hello world"
+print(len(t))
+a=input("enter you name ")
+print(len(a))
+t="python"
+print(t.upper())
+print(t.lower())
+print(t.capitalize())
+print(t.title())
+print(t.replace("pyth","jav"))
