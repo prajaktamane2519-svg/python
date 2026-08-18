b097ca34d164638260b1df76d141310fdb9d684b
Prajakta mane <prajaktamane2519@gmail.com>
2026-08-18 11:14:35 +0530

    ifprogram

---
+++ if.py
@@ -0,0 +1,40 @@
+num=int(input("enter the number :"))
+if num>0:
+    print("positive number")
+else:
+    print("Negative number")
+num=int(input("enter the number :"))
+if num%2==0:
+    print("even number")
+else:
+    print("odd number")
+num=int(input("enter the number :"))
+if num>=18:
+    print("eligible to vote")
+else:
+    print("not eligible to vote")
+num=100
+if num>100:
+    print("greater than 100")
+elif num<0:
+    print("less than 100")
+else:
+    print("equal to 100")
+num=int(input("enter the number :"))
+if num>35:
+    print("pass")
+else:
+    print("Fail")
+num=int(input("enter the number :"))
+if num%5==0:
+    print("Divisible by 5")
+else:
+    print("not divisible 5")
+
+marks=80
+if marks>75:
+    print("high")
+elif marks>50:
+    print("medium")
+else:
+    print("low")
