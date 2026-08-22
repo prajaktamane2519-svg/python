66516598f6e0c3c4b38e57a73d44f257b2d3007e
Prajakta mane <prajaktamane2519@gmail.com>
2026-08-22 11:43:31 +0530

    practiceprogram

--- practice.py
+++ practice.py
@@ -10,3 +10,58 @@ balance=5000
 if pin==1234:
     if balance>0:
         print("TRansaction allowed")
+marks=60
+attendance=78
+if marks>=60:
+    if attendance>=75:
+        print("Eligible")
+username="admin"
+password="1234"
+if username=="admin":
+    if password=="1234":
+        print("login succeful")
+for i in (1,10):
+    print(i)
+for i in (1,10,2):
+    print(i)
+for i in range(5):
+    print("praj")
+for i in range(1,11):
+    total=+i
+print(total)
+i=1
+while i<=5:
+    print(i)
+    i+=1
+i=2 
+while i<=10:
+    print(i)
+    i+=2
+i=1
+total=0
+while i<=5:
+    total+=1
+    i+=1
+print(total)
+for i in range(1,10):
+    if i==5:
+        break
+    print(i)
+name="praj"
+print(len(name))
+name='Praj'
+print(name.upper())
+print(name.lower())
+
+print(name[1::3])
+print(name[::-1])
+numbers=[1,3,6,7,8]
+print(numbers)
+numbers.append(20)
+print(numbers)
+numbers.remove(6)
+print(numbers)
+
+
+
+     
