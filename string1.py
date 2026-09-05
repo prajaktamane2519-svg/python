3557120687514cec2fa40de1a2f5e7b5d9ce2eb1
Prajakta mane <prajaktamane2519@gmail.com>
2026-09-05 19:36:26 +0530

    string program

--- string1.py
+++ string1.py
@@ -24,3 +24,35 @@ print(s.isalnum())
 print(s[::-1])
 for ch in s:
     print(ch)
+s="python"
+count=0
+for ch in s:
+    if ch in "aeiou":
+        count+=1
+print(count)
+s="python"
+count=0
+for ch in s:
+    if ch.isalpha()and ch not in "aeiou":
+        count+=1
+print(count)
+s="i  love python "
+print(s.count(" "))
+w=s.split()
+print(len(w))
+print(s.replace(" ",""))
+s=" i love python"
+print(s.replace(" ","_"))
+s="madam"
+if s== s[::-1]:
+    print("s is palidrom")
+else:
+    print("s is not")
+s1="python"
+s2="python"
+if s1==s2:
+    print("equal")
+else:
+    print("not equqal")
+s="python is easy"
+print(s.title())
