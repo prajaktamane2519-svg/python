ef8f8ba60221152c690b429c06222cc7952267a9
Prajakta mane <prajaktamane2519@gmail.com>
2026-09-08 18:37:33 +0530

    practice program

--- practice2.py
+++ practice2.py
@@ -36,3 +36,27 @@ if a>b:
     print("largest")
 else:
     print("smallest")
+a=100
+r=5
+t=2
+si=(a*r*t)/100
+print(si)
+a=int(input("enter the number"))
+if a>0:
+    print("positive")
+else:
+    print("negative")
+a=int(input("enter the number"))
+if a%2==0:
+    print("even")
+else:
+    print("odd")
+marks=78
+if marks>=90:
+    print("A")
+elif marks>=80:
+    print("B")
+elif marks>=60:
+    print("c")
+else:
+    print("D")
